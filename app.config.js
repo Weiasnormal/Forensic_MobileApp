36c955bdd45d0abb0f977841186ced8fae295d8a
Weiasnormal <crusitwincel@gmail.com>
2026-07-04 12:11:38 +0800

    Fixed using expo doctor

--- app.config.js
+++ app.config.js
@@ -1,6 +1,9 @@
-export default {
- 
-  extra: {
-    apiBaseUrl: process.env.EXPO_PUBLIC_AVERA_API_BASE_URL,
-  },
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      apiBaseUrl: process.env.EXPO_PUBLIC_AVERA_API_BASE_URL,
+    },
+  };
 };
